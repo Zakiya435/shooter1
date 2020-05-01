6c1f741ecebfeb1042d289970d3c2bbba28f4dd0
Zakiya435 <63589526+Zakiya435@users.noreply.github.com>
2020-05-01 12:41:06 +0530

    Update Tanker.js

--- Tanker.js
+++ Tanker.js
@@ -4,7 +4,7 @@ class Tanker {
       isStatic: true
       
      }
-     this.image = loadImage("assets/shooter.png");
+     this.image = loadImage("shooter.png");
      this.body = Bodies.rectangle(x,y,width,height,options)
      World.add(world, this.body);
 
